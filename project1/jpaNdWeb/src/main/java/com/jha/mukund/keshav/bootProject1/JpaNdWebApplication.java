12757c43936bfcb7550e5c438283bf88fab9fb35
Keshav Mukund <keshavmukundjha@gmail.com>
2023-08-08 03:17:42 +0530

    Initial state of project1 after creation, JDK19 base

---
+++ project1/jpaNdWeb/src/main/java/com/jha/mukund/keshav/bootProject1/JpaNdWebApplication.java
@@ -0,0 +1,13 @@
+package com.jha.mukund.keshav.bootProject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaNdWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaNdWebApplication.class, args);
+	}
+
+}
